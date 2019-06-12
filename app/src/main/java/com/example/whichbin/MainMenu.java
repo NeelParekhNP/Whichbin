468e3485f322a6c5f4853bc5d2799dcf5cf6354f
LHS3000 <lawrence.h.smith.89@gmail.com>
2019-06-12 16:23:10 +0100

    Main menu changes for the buttons to select between games.

---
+++ app/src/main/java/com/example/whichbin/MainMenu.java
@@ -0,0 +1,96 @@
+package com.example.whichbin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainMenu extends AppCompatActivity {
+    String msg = "Android : ";
+    private Button whichBinButton;
+    private Button triviaButton;
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        Log.d(msg, "The onCreate() event");
+
+        whichBinButton = (Button) findViewById(R.id.whichBinButtonX);
+        whichBinButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openBinGame();
+            }
+        });
+
+        triviaButton = (Button) findViewById(R.id.triviaGameX);
+        triviaButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openTriviaGame();
+            }
+        });
+    }
+    /** Called if the Which Bin game button's clicked. */
+    public void openBinGame(){
+        Intent intent = new Intent(this, BinGame.class);
+        startActivity(intent);
+    }
+
+    public void openTriviaGame(){
+        Intent intent = new Intent(this, TriviaGame.class);
+        startActivity(intent);
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
