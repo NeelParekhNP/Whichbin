f7fa9363a44652cd496605c57638e6f02d3da81e
LHS3000 <lawrence.h.smith.89@gmail.com>
2019-06-12 16:10:53 +0100

    Not all changes were updated in the previous push somehow-pushing again so hopefully the whole app is put onto GitHub.

---
+++ app/src/main/java/com/example/whichbin/TriviaGame.java
@@ -0,0 +1,13 @@
+package com.example.whichbin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TriviaGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia_game);
+    }
+}
