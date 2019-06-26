e9a0649029f3e5e9b43773530fc15a10fe79cd7f
LHS3000 <lawrence.h.smith.89@gmail.com>
2019-06-26 11:42:13 +0100

    Got a working prototype of the true or false game working with a score keeper. Currently it loops around back to the beginning so need to add a new activity with the correct answers at the end.

--- app/src/main/java/com/example/whichbin/TriviaGame.java
+++ app/src/main/java/com/example/whichbin/TriviaGame.java
@@ -11,62 +11,90 @@ public class TriviaGame extends AppCompatActivity {
 
     private TriviaGameManager triviaGameManager = new TriviaGameManager();
     private TextView questionDisplay;
+    private TextView scoreDisplay;
     private boolean inputAnswer;
-    private int questionNumber = 0;
+    private int nextQuestionNumber = 0;
     private Button trueButton;
     private Button falseButton;
+    private String displayQuestion;
     private String responseToast;
+    private String triviaQuestion;
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia_game);
 
-        trueButton = (Button) findViewById(R.id.triviaTrueButton);
+
+        questionDisplay = (TextView) findViewById(R.id.trivia_question);
+        scoreDisplay = (TextView) findViewById(R.id.score_int);
+        trueButton = (Button) findViewById(R.id.trivia_true_button);
+        falseButton = (Button) findViewById(R.id.trivia_false_button);
+
+        displayNextQuestion();
+        incrementScore();
+
         trueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 inputAnswer = true;
-                responseToast = checkIfCorrect(questionNumber, inputAnswer);
-                Toast.makeText(getApplicationContext(), responseToast, Toast.LENGTH_LONG).show();
+                responseToast = checkIfCorrect(nextQuestionNumber, inputAnswer);
+                Toast.makeText(getApplicationContext(), responseToast, Toast.LENGTH_SHORT).show();
+                displayNextQuestion();
             }
         });
 
-        falseButton = (Button) findViewById(R.id.triviaFalseButton);
         falseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 inputAnswer = false;
-                String responseToast = checkIfCorrect(questionNumber, inputAnswer);
-                Toast.makeText(getApplicationContext(), responseToast, Toast.LENGTH_LONG).show();
+                String responseToast = checkIfCorrect(nextQuestionNumber, inputAnswer);
+                Toast.makeText(getApplicationContext(), responseToast, Toast.LENGTH_SHORT).show();
+                displayNextQuestion();
             }
         });
 
     }
 
-    public void displayNextQuestion(int currentQuestion){
-        int nextQuestion = currentQuestion + 1;
-        formatQuestion(nextQuestion);
 
+
+    /** Method to increment the question-currently cycles back to the beginning after 10 questions.*/
+    private void displayNextQuestion(){
+        if(nextQuestionNumber < 10){
+            triviaQuestion = triviaGameManager.getTriviaQuestionByNo(nextQuestionNumber);
+            questionDisplay.setText(triviaQuestion);
+            nextQuestionNumber++;
+        }
+        // For now I'll just have the questions loop back to the beginning instead of the app crashing hence resetting questionNumber
+        else{
+            nextQuestionNumber = 0;
+            triviaQuestion = triviaGameManager.getTriviaQuestionByNo(nextQuestionNumber);
+            questionDisplay.setText(triviaQuestion);
+            nextQuestionNumber++;
+        }
     }
 
-
-
-
-    public String formatQuestion(int qNo){
-        String formattedQuestion = "q" + qNo;
-        return formattedQuestion;
-    }
-
-    public String checkIfCorrect(int qNo, boolean inputAnswer){
-        triviaGameManager.getTriviaQuestionByNo(qNo);
-        boolean actualAnswer = triviaGameManager.checkTriviaQuestionAnswer(qNo);
-        if(actualAnswer = inputAnswer){
+    private String checkIfCorrect(int qNo, boolean inputAnswer){
+        boolean actualAnswer = getAnswer(qNo);
+        if(actualAnswer == inputAnswer){
+            incrementScore();
             return "That's the right answer!";
         }
         else{
             return "That's the wrong answer...";
         }
+    }
+
+    private boolean getAnswer(int qNo){
+        int qNoLessOne = qNo-1;
+        boolean answer = triviaGameManager.checkTriviaQuestionAnswer(qNoLessOne);
+        return answer;
+    }
+
+    private void incrementScore(){
+        scoreDisplay.setText(String.valueOf(score));
+        score++;
     }
 }
 
