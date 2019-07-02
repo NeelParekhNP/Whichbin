801ce7f9e3a7fb590049d9f320df0f974f1fe375
LHS3000 <lawrence.h.smith.89@gmail.com>
2019-07-02 18:32:45 +0100

    Added a timer to the trivia game and linked it to a new activity when all ten answers are given or if the 2 minutes run out (whichever happens first). This new activity currently just says "well done" but I will work on getting it to display answers.

--- app/src/main/java/com/example/whichbin/TriviaGame.java
+++ app/src/main/java/com/example/whichbin/TriviaGame.java
@@ -1,5 +1,7 @@
 package com.example.whichbin;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,8 @@ public class TriviaGame extends AppCompatActivity {
     private TriviaGameManager triviaGameManager = new TriviaGameManager();
     private TextView questionDisplay;
     private TextView scoreDisplay;
+    private TextView timeDisplay;
+
     private boolean inputAnswer;
     private int nextQuestionNumber = 0;
     private Button trueButton;
@@ -20,6 +24,8 @@ public class TriviaGame extends AppCompatActivity {
     private String responseToast;
     private String triviaQuestion;
     private int score = 0;
+    private CountDownTimer clock;
+    private long timeRemaining = 120000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +35,13 @@ public class TriviaGame extends AppCompatActivity {
 
         questionDisplay = (TextView) findViewById(R.id.trivia_question);
         scoreDisplay = (TextView) findViewById(R.id.score_int);
+        timeDisplay = (TextView) findViewById(R.id.timer_time);
         trueButton = (Button) findViewById(R.id.trivia_true_button);
         falseButton = (Button) findViewById(R.id.trivia_false_button);
 
         displayNextQuestion();
         incrementScore();
+        startTimer();
 
         trueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -68,10 +76,15 @@ public class TriviaGame extends AppCompatActivity {
         }
         // For now I'll just have the questions loop back to the beginning instead of the app crashing hence resetting questionNumber
         else{
+
+            /**
             nextQuestionNumber = 0;
             triviaQuestion = triviaGameManager.getTriviaQuestionByNo(nextQuestionNumber);
             questionDisplay.setText(triviaQuestion);
             nextQuestionNumber++;
+            */
+
+            openAnswerActivity();
         }
     }
 
@@ -92,9 +105,45 @@ public class TriviaGame extends AppCompatActivity {
         return answer;
     }
 
+    private void startTimer(){
+        clock = new CountDownTimer(timeRemaining, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeRemaining = millisUntilFinished;
+                getTimeString();
+            }
+
+            @Override
+            public void onFinish() {
+                openAnswerActivity();
+            }
+        }.start();
+    }
+
+    private void getTimeString(){
+        int mins = (int) timeRemaining / 60000;
+        int secs = (int) timeRemaining % 60000 /1000;
+
+        String timeString;
+
+        timeString = "" + mins;
+        timeString += ":";
+        if(secs < 10) {
+            timeString += "0";
+        }
+        timeString += secs;
+
+        timeDisplay.setText(timeString);
+    }
+
     private void incrementScore(){
         scoreDisplay.setText(String.valueOf(score));
         score++;
+    }
+
+    public void openAnswerActivity(){
+        Intent intent = new Intent(this, TriviaAnswerActivity.class);
+        startActivity(intent);
     }
 }
 
