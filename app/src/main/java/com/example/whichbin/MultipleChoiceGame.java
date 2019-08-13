bc25171fad06d1edf8a5fee8498fcdf42afe62a3
NeelParekhNP <47748376+NeelParekhNP@users.noreply.github.com>
2019-08-13 20:04:03 +0100

    Added more questions to multiple choice game.

--- app/src/main/java/com/example/whichbin/MultipleChoiceGame.java
+++ app/src/main/java/com/example/whichbin/MultipleChoiceGame.java
@@ -30,7 +30,32 @@ public class MultipleChoiceGame extends AppCompatActivity {
             new MultiplayerGameQuestions(R.string.m_question_eight, 2, "0.53 degrees", "0.94 degrees", "1.34 degrees", "0.21 degrees"),
             new MultiplayerGameQuestions(R.string.m_question_nine, 3, "Jupiter", "Mars", "Venus", "Earth"),
             new MultiplayerGameQuestions(R.string.m_question_ten, 3, "10,500", "1,500,000","150,000", "1,500")
+    };
 
+    private MultiplayerGameQuestions[] scienceQuestions = new MultiplayerGameQuestions[]{
+            new MultiplayerGameQuestions(R.string.s_mcq_question_one, 2,"Carbon Dioxide", "Nitrogen", "Water Vapour", "Methane"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_two, 1,"17 to 22℃ cooler", "17 to 22℃ warmer", "6 to 11℃ cooler", "6 to 11℃ warmer"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_three, 4, "Using remote sensing from space with satellites", "By ground-based measurements of surface temperature, carbon dioxide concentration and sea level", "By collecting \"proxy data\" from tree rings, ice cores and historical records", "All of the above"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_four, 4,"Carbon Dioxide", "Methane", "Water Vapour", "All of the above"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_five, 1,"Forests serve as a sink in the carbon cycle", "Trees provide building materials", "Trees are an important food source", "Leaves of trees reflect all sunlight away from the Earth"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_six, 2,"A bicycle made of carbon fibre", "A cycle in which carbon flows between the atmosphere, land, and ocean", "Water Vapour", "Methane"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_seven, 1,"17 to 22℃ cooler", "17 to 22℃ warmer", "6 to 11℃ cooler", "6 to 11℃ warmer"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_eight, 4, "Using remote sensing from space with satellites", "By ground-based measurements of surface temperature, carbon dioxide concentration and sea level", "By collecting \"proxy data\" from tree rings, ice cores and historical records", "All of the above"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_nine, 4,"Carbon Dioxide", "Methane", "Water Vapour", "All of the above"),
+            new MultiplayerGameQuestions(R.string.s_mcq_question_ten, 4,"Carbon Dioxide", "Methane", "Water Vapour", "All of the above")
+    };
+
+    private MultiplayerGameQuestions[] natureQuestions = new MultiplayerGameQuestions[]{
+            new MultiplayerGameQuestions(R.string.n_mcq_question_one, 1,"They aren’t as hungry because it’s so hot", "Warmer water makes them sleepy", "Their migratory patterns are changing", "Summer seems longer so fish are schooling less"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_two, 4,"Water shortages", "Power outages", "Higher electricity and gas prices", "All of the above"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_three, 3, "Too many people with the same name slows emergency response", "There are not enough celebrities focused on climate issues", "The need to upgrade aging infrastructure", "People can’t identify the needed solutions so take no action"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_four, 3,"Increasing extreme precipitation everywhere", "Increasing length of droughts everywhere", "Increase in both wet and dry extremes", "Climate change doesn’t alter precipitation"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_five, 2,"Heavy rain increases the risk of animals transmitting rabies", "Milder winters increase the tick density & risk of lyme", "Dry summers & drought increase the risk of West Nile virus", "Snow storms increase mosquito density & risk of malaria"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_six, 2,"Carbon Dioxide", "Nitrogen", "Water Vapour", "Methane"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_seven, 1,"17 to 22℃ cooler", "17 to 22℃ warmer", "6 to 11℃ cooler", "6 to 11℃ warmer"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_eight, 4, "Using remote sensing from space with satellites", "By ground-based measurements of surface temperature, carbon dioxide concentration and sea level", "By collecting \"proxy data\" from tree rings, ice cores and historical records", "All of the above"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_nine, 4,"Carbon Dioxide", "Methane", "Water Vapour", "All of the above"),
+            new MultiplayerGameQuestions(R.string.n_mcq_question_ten, 4,"Carbon Dioxide", "Methane", "Water Vapour", "All of the above")
     };
 
     @Override
