6c29e90d0a888dadb6ce65d7c1f5700bb6b6c051
NeelParekhNP <47748376+NeelParekhNP@users.noreply.github.com>
2019-08-15 21:23:06 +0100

    Completed all the zones for the drag and drop game and made some changes to the recycling zone.

--- app/src/main/java/com/example/whichbin/BinGame.java
+++ app/src/main/java/com/example/whichbin/BinGame.java
@@ -34,17 +34,16 @@ public class BinGame extends AppCompatActivity {
     public static final String LEVEL_ONE_WORLD_THREE_STATUS = "levelOneWorldThreeStatus";
 
     private BinGameQuestions[] recycleQuestions = new BinGameQuestions[]{
-            new BinGameQuestions(R.string.question_zero, 1, R.drawable.image_0),
-            new BinGameQuestions(R.string.question_one, 2, R.drawable.image_1),
-            new BinGameQuestions(R.string.question_two, 3, R.drawable.image_2),
-            new BinGameQuestions(R.string.question_three, 2, R.drawable.image_3),
-            new BinGameQuestions(R.string.question_four, 3, R.drawable.image_4),
-            new BinGameQuestions(R.string.question_five, 3, R.drawable.image_5),
-            new BinGameQuestions(R.string.question_six, 1, R.drawable.image_6),
-            new BinGameQuestions(R.string.question_seven, 1, R.drawable.image_7),
-            new BinGameQuestions(R.string.question_eight, 1, R.drawable.image_8),
-            new BinGameQuestions(R.string.question_nine, 1, R.drawable.image_9),
-            new BinGameQuestions(R.string.question_ten, 1, R.drawable.image_10)
+            new BinGameQuestions(R.string.question_one, 3, R.drawable.carrot_organic),
+            new BinGameQuestions(R.string.question_two, 1, R.drawable.styrofoam_trash),
+            new BinGameQuestions(R.string.question_three, 3, R.drawable.strawberry_organic),
+            new BinGameQuestions(R.string.question_four, 2, R.drawable.newspaper_recycle),
+            new BinGameQuestions(R.string.question_five, 2, R.drawable.can_recycle),
+            new BinGameQuestions(R.string.question_six, 1, R.drawable.crisps_packet_trash),
+            new BinGameQuestions(R.string.question_seven, 2, R.drawable.bottle_recycle),
+            new BinGameQuestions(R.string.question_eight, 1, R.drawable.plate_trash),
+            new BinGameQuestions(R.string.question_nine, 2, R.drawable.paper_bag_recycle),
+            new BinGameQuestions(R.string.question_ten, 3, R.drawable.apple_oraganic)
     };
 
     private BinGameQuestions[] conservationStatusQuestions = new BinGameQuestions[]{
@@ -58,6 +57,20 @@ public class BinGame extends AppCompatActivity {
             new BinGameQuestions(R.string.animal_question_eight,1,R.drawable.giant_panda_vulnerable),
             new BinGameQuestions(R.string.animal_question_nine,3,R.drawable.nubian_giraffe_ce),
             new BinGameQuestions(R.string.animal_question_ten,3,R.drawable.adriatic_sturgeon_ce)
+    };
+
+    private BinGameQuestions[] impactQuestions = new BinGameQuestions[]{
+            new BinGameQuestions(R.string.impact_question_one,1,R.drawable.gasoline_high),
+            new BinGameQuestions(R.string.impact_question_two,3,R.drawable.vegan_low),
+            new BinGameQuestions(R.string.impact_question_three,1,R.drawable.deforestation_high),
+            new BinGameQuestions(R.string.impact_question_four,2,R.drawable.light_medium),
+            new BinGameQuestions(R.string.impact_question_five,3,R.drawable.bicycle_low),
+            new BinGameQuestions(R.string.impact_question_six,1,R.drawable.meat_high),
+            new BinGameQuestions(R.string.impact_question_seven,3,R.drawable.solar_power_low),
+            new BinGameQuestions(R.string.impact_question_eight,2,R.drawable.vegeterian_medium),
+            new BinGameQuestions(R.string.impact_question_nine,2,R.drawable.bus_medium),
+            new BinGameQuestions(R.string.impact_question_ten,1,R.drawable.car_high),
+
     };
 
     @Override
@@ -95,19 +108,18 @@ public class BinGame extends AppCompatActivity {
                 option2.setImageDrawable(getDrawable(R.drawable.endangered_box));
                 option3.setImageDrawable(getDrawable(R.drawable.critically_endangered_box));
                 header = "Which box does this " + getString(questionSet[currentIndex].getQuestion()) + " belong in?";
-                option1Label.setText("Vulnerable Species");
+                nameTags.setVisibility(View.GONE);
+/*                option1Label.setText("Vulnerable Species");
                 option2Label.setText("Endangered Species");
-                option3Label.setText("Critically Endangered Species");
+                option3Label.setText("Critically Endangered Species");*/
                 break;
             case 3 :
-                questionSet = conservationStatusQuestions;
-                option1.setImageDrawable(getDrawable(R.drawable.vulnerable_box));
-                option2.setImageDrawable(getDrawable(R.drawable.endangered_box));
-                option3.setImageDrawable(getDrawable(R.drawable.critically_endangered_box));
-                header = "Which box does this " + getString(questionSet[currentIndex].getQuestion()) + " belong in?";
-                option1Label.setText("Vulnerable Species");
-                option2Label.setText("Endangered Species");
-                option3Label.setText("Critically Endangered Species");
+                questionSet = impactQuestions;
+                option1.setImageDrawable(getDrawable(R.drawable.high_impact_sign));
+                option2.setImageDrawable(getDrawable(R.drawable.medium_impact_sign));
+                option3.setImageDrawable(getDrawable(R.drawable.low_impact_sign));
+                header = "How much does " + getString(questionSet[currentIndex].getQuestion()) + " impact climate change?";
+                nameTags.setVisibility(View.GONE);
                 break;
             default:
                     questionSet = recycleQuestions;
@@ -196,6 +208,8 @@ public class BinGame extends AppCompatActivity {
                             case 2 :
                                 header = "Which box does this " + getString(question) + " belong in?";
                                 break;
+                            case 3:
+                                header ="How much does " + getString(question) + " impact climate change?";
                         }
                         questionTextView.setText(header);
                     }
