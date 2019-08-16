f01656843dd66628bb22808736995e413efb65e0
NeelParekhNP <47748376+NeelParekhNP@users.noreply.github.com>
2019-08-16 16:16:10 +0100

    Added feedback at the end of drag and drop game.

--- app/src/main/java/com/example/whichbin/BinGame.java
+++ app/src/main/java/com/example/whichbin/BinGame.java
@@ -20,7 +20,7 @@ import android.widget.Toast;
 public class BinGame extends AppCompatActivity {
 
     private ImageView question, option1, option2, option3;
-    private TextView questionTextView, option1Label, option2Label, option3Label;
+    private TextView questionTextView, option1Label, option2Label, option3Label, resultsView;
     private LinearLayout allOptions, nameTags;
     private int currentIndex = 0;
     private int totalCorrect = 0;
@@ -28,48 +28,49 @@ public class BinGame extends AppCompatActivity {
 
     private int levelTheme;
     private BinGameQuestions[] questionSet;
+    private int [] userAnswers = new int[10];
 
     public static final String LEVEL_ONE_WORLD_ONE_STATUS = "levelOneWorldOneStatus";
     public static final String LEVEL_ONE_WORLD_TWO_STATUS = "levelOneWorldTwoStatus";
     public static final String LEVEL_ONE_WORLD_THREE_STATUS = "levelOneWorldThreeStatus";
 
     private BinGameQuestions[] recycleQuestions = new BinGameQuestions[]{
-            new BinGameQuestions(R.string.question_one, 3, R.drawable.carrot_organic),
-            new BinGameQuestions(R.string.question_two, 1, R.drawable.styrofoam_trash),
-            new BinGameQuestions(R.string.question_three, 3, R.drawable.strawberry_organic),
-            new BinGameQuestions(R.string.question_four, 2, R.drawable.newspaper_recycle),
-            new BinGameQuestions(R.string.question_five, 2, R.drawable.can_recycle),
-            new BinGameQuestions(R.string.question_six, 1, R.drawable.crisps_packet_trash),
-            new BinGameQuestions(R.string.question_seven, 2, R.drawable.bottle_recycle),
-            new BinGameQuestions(R.string.question_eight, 1, R.drawable.plate_trash),
-            new BinGameQuestions(R.string.question_nine, 2, R.drawable.paper_bag_recycle),
-            new BinGameQuestions(R.string.question_ten, 3, R.drawable.apple_oraganic)
+            new BinGameQuestions(R.string.question_one, 3, R.drawable.carrot_organic, "Carrots are organic waste"),
+            new BinGameQuestions(R.string.question_two, 1, R.drawable.styrofoam_trash, "Styrofoam isn't currently recyclable."),
+            new BinGameQuestions(R.string.question_three, 3, R.drawable.strawberry_organic, "Strawberries are organic waste"),
+            new BinGameQuestions(R.string.question_four, 2, R.drawable.newspaper_recycle, "Newspaper can be recycled"),
+            new BinGameQuestions(R.string.question_five, 2, R.drawable.can_recycle, "Cans are normally made of aluminium which can be recycled"),
+            new BinGameQuestions(R.string.question_six, 1, R.drawable.crisps_packet_trash, "Crisp packets are made of a \"metallised plastic\" that can't be recycled"),
+            new BinGameQuestions(R.string.question_seven, 2, R.drawable.bottle_recycle, "Plastic bottles can be recycled"),
+            new BinGameQuestions(R.string.question_eight, 1, R.drawable.plate_trash, "Plates are made of ceramic which can't be recycled"),
+            new BinGameQuestions(R.string.question_nine, 2, R.drawable.paper_bag_recycle, "Paper bags can be recycled"),
+            new BinGameQuestions(R.string.question_ten, 3, R.drawable.apple_oraganic, "Apples are organic waste")
     };
 
     private BinGameQuestions[] conservationStatusQuestions = new BinGameQuestions[]{
-            new BinGameQuestions(R.string.animal_question_one,1,R.drawable.leatherback_turtle_vulnerable),
-            new BinGameQuestions(R.string.animal_question_two,1,R.drawable.white_rhino_vulnerable),
-            new BinGameQuestions(R.string.animal_question_three,3,R.drawable.sumatran_orangutan_ce),
-            new BinGameQuestions(R.string.animal_question_four,2,R.drawable.white_tiger_endangered),
-            new BinGameQuestions(R.string.animal_question_five,3,R.drawable.amsterdam_albatross_ce),
-            new BinGameQuestions(R.string.animal_question_six,2,R.drawable.beluga_whale_endangered),
-            new BinGameQuestions(R.string.animal_question_seven,2,R.drawable.asian_elephant_endangered),
-            new BinGameQuestions(R.string.animal_question_eight,1,R.drawable.giant_panda_vulnerable),
-            new BinGameQuestions(R.string.animal_question_nine,3,R.drawable.nubian_giraffe_ce),
-            new BinGameQuestions(R.string.animal_question_ten,3,R.drawable.adriatic_sturgeon_ce)
+            new BinGameQuestions(R.string.animal_question_one,1,R.drawable.leatherback_turtle_vulnerable, "Leatherback turtles are considered vulnerable. There are only 34,000 - 36,000 nesting females left worldwide."),
+            new BinGameQuestions(R.string.animal_question_two,1,R.drawable.white_rhino_vulnerable, "White rhinos are considered vulnerable. However, they are a conservation success story because a few years ago they were nearly instinct."),
+            new BinGameQuestions(R.string.animal_question_three,3,R.drawable.sumatran_orangutan_ce, "Sumatran Orangutan are critically endangered. There are only approximately 14,613 left in the wild. "),
+            new BinGameQuestions(R.string.animal_question_four,2,R.drawable.white_tiger_endangered, "White tigers are endangered. There are none left in wild and only approximately 200 in captivity."),
+            new BinGameQuestions(R.string.animal_question_five,3,R.drawable.amsterdam_albatross_ce, "Amsterdam albatross are critically endangered. There are only approximately 130 left. "),
+            new BinGameQuestions(R.string.animal_question_six,2,R.drawable.beluga_whale_endangered, "Beluga whales are endangered. Their population size is between 279 and 386 animals"),
+            new BinGameQuestions(R.string.animal_question_seven,2,R.drawable.asian_elephant_endangered, "Asian elephants are endangered. There are only 35,000 - 40,000 left in the wild."),
+            new BinGameQuestions(R.string.animal_question_eight,1,R.drawable.giant_panda_vulnerable, "Giant pandas are vulnerable. Like the white rhino, giant pandas are a success story as their numbers have increased by 17% since the 1970's."),
+            new BinGameQuestions(R.string.animal_question_nine,3,R.drawable.nubian_giraffe_ce, "Nubian giraffes are critically endangered. It's estiamted that there are only 2,150 left in the wild."),
+            new BinGameQuestions(R.string.animal_question_ten,3,R.drawable.adriatic_sturgeon_ce, "Adritaic sturgeon are critically endangered. They're functionally extinct in the wild, but they do bredd in captivity.")
     };
 
     private BinGameQuestions[] impactQuestions = new BinGameQuestions[]{
-            new BinGameQuestions(R.string.impact_question_one,1,R.drawable.gasoline_high),
-            new BinGameQuestions(R.string.impact_question_two,3,R.drawable.vegan_low),
-            new BinGameQuestions(R.string.impact_question_three,1,R.drawable.deforestation_high),
-            new BinGameQuestions(R.string.impact_question_four,2,R.drawable.light_medium),
-            new BinGameQuestions(R.string.impact_question_five,3,R.drawable.bicycle_low),
-            new BinGameQuestions(R.string.impact_question_six,1,R.drawable.meat_high),
-            new BinGameQuestions(R.string.impact_question_seven,3,R.drawable.solar_power_low),
-            new BinGameQuestions(R.string.impact_question_eight,2,R.drawable.vegeterian_medium),
-            new BinGameQuestions(R.string.impact_question_nine,2,R.drawable.bus_medium),
-            new BinGameQuestions(R.string.impact_question_ten,1,R.drawable.car_high),
+            new BinGameQuestions(R.string.impact_question_one,1,R.drawable.gasoline_high, "Fossil fuels release carbon dioxide, a greenhouse gas into the atmosphere. Therefore, it has a high impact on climate change."),
+            new BinGameQuestions(R.string.impact_question_two,3,R.drawable.vegan_low, "Being vegan means animal products are not consumed. This means it will have a low impact on climate change."),
+            new BinGameQuestions(R.string.impact_question_three,1,R.drawable.deforestation_high, "Trees remove carbon dioxide from the atmosphere and convert it into oxygen. Therefore, deforestation will have a large impact because less carbon dioxide will be removed from the atmosphere. "),
+            new BinGameQuestions(R.string.impact_question_four,2,R.drawable.light_medium, "Leaving lights on means electricity is wasted. Therefore, it has a medium impact on climate change."),
+            new BinGameQuestions(R.string.impact_question_five,3,R.drawable.bicycle_low, "Riding bikes have a low impact on climate change because they don't use any sort of fuel."),
+            new BinGameQuestions(R.string.impact_question_six,1,R.drawable.meat_high, "Eating meat has a high impact because animals need to be raised for food. Animals produce a lot of methane which is a greenhouse gas."),
+            new BinGameQuestions(R.string.impact_question_seven,3,R.drawable.solar_power_low, "Solar power is a low impact method of producing electricity because it uses renewable resources."),
+            new BinGameQuestions(R.string.impact_question_eight,2,R.drawable.vegeterian_medium, "Being vegetarian has a medium impact because animals don't need to be raised for food, but some animals still need to be raised for milk."),
+            new BinGameQuestions(R.string.impact_question_nine,2,R.drawable.bus_medium, "Travelling on buses have a medium impact because even though they burn fuels, they carry more people so it's not as wasteful."),
+            new BinGameQuestions(R.string.impact_question_ten,1,R.drawable.car_high, "Cars have a huge impact on climate change. On average cars only carry five people and burn a lot of fuel."),
 
     };
 
@@ -90,6 +91,7 @@ public class BinGame extends AppCompatActivity {
         option1Label = (TextView) findViewById(R.id.textView_option1_tag);
         option2Label = (TextView) findViewById(R.id.textView_option2_tag);
         option3Label = (TextView) findViewById(R.id.textView_option3_tag);
+        resultsView = (TextView) findViewById(R.id.textView_DND_game_results);
 
         switch (levelTheme){
             case 1 :
@@ -101,6 +103,7 @@ public class BinGame extends AppCompatActivity {
                 option1Label.setText("General Waste");
                 option2Label.setText("Recycle");
                 option3Label.setText("Organic Waste");
+                resultsView.setTextSize(18);
                 break;
             case 2 :
                 questionSet = conservationStatusQuestions;
@@ -109,6 +112,7 @@ public class BinGame extends AppCompatActivity {
                 option3.setImageDrawable(getDrawable(R.drawable.critically_endangered_box));
                 header = "Which box does this " + getString(questionSet[currentIndex].getQuestion()) + " belong in?";
                 nameTags.setVisibility(View.GONE);
+                resultsView.setTextSize(16);
 /*                option1Label.setText("Vulnerable Species");
                 option2Label.setText("Endangered Species");
                 option3Label.setText("Critically Endangered Species");*/
@@ -120,6 +124,7 @@ public class BinGame extends AppCompatActivity {
                 option3.setImageDrawable(getDrawable(R.drawable.low_impact_sign));
                 header = "How much does " + getString(questionSet[currentIndex].getQuestion()) + " impact climate change?";
                 nameTags.setVisibility(View.GONE);
+                resultsView.setTextSize(14);
                 break;
             default:
                     questionSet = recycleQuestions;
@@ -127,6 +132,8 @@ public class BinGame extends AppCompatActivity {
         questionTextView.setText(header);
 
         question.setImageDrawable(getDrawable(questionSet[currentIndex].getImage()));
+
+        resultsView.setVisibility(View.INVISIBLE);
 
         option1.setOnDragListener(dragListener);
         option2.setOnDragListener(dragListener);
@@ -173,6 +180,7 @@ public class BinGame extends AppCompatActivity {
 
                     int answer = questionSet[currentIndex].isAnswer();
                     int tagDropTarget = Integer.parseInt((String)dropTarget.getTag());
+                    userAnswers[currentIndex] = tagDropTarget;
 
                     if (answer == tagDropTarget ){
                         Toast.makeText(BinGame.this,R.string.correctMessage, Toast.LENGTH_SHORT).show();
@@ -192,6 +200,8 @@ public class BinGame extends AppCompatActivity {
                         LinearLayout ll = (LinearLayout)findViewById(R.id.optionsLayout);
                         ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                         ll.addView(menuButton, lp);
+
+                        displayAnswers();
 
                         menuButton.setOnClickListener(clickListener);
                     }
@@ -218,6 +228,21 @@ public class BinGame extends AppCompatActivity {
             return true;
         }
     };
+
+    private void displayAnswers(){
+        String results = "";
+        for(int i = 0; i < userAnswers.length; i++){
+            if(userAnswers[i] == questionSet[i].isAnswer()){
+                results += "" + (i+1) + ")" + " Correct! - " + questionSet[i].getAnswer()+"\n";
+            }
+            else {
+                results += "" + (i+1) + ")" + " Incorrect! - " + questionSet[i].getAnswer()+"\n";
+            }
+        }
+
+        resultsView.setVisibility(View.VISIBLE);
+        resultsView.setText(results);
+    }
 
     private void saveData() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
